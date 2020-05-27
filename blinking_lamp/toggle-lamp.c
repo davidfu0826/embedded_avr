38e1ceae053d66990143d3bb8dff23d00e6ea591
davidfu0826 <david.bao.fu@gmail.com>
2020-05-27 10:24:52 +0100

    Add first files

---
+++ blinking_lamp/toggle-lamp.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+
+int main (void)
+{
+    while (1)
+    {
+        /* Check if button is pressed */
+        if ()
+        {
+            /* Change state */
+        }
+
+        /* Check if state is TURN ON LAMP */
+        if ()
+        {
+            /* Turn on lamp */
+        }
+    }
+}
